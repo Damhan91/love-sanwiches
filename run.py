d11f38927bf9a2c64db034ea8e96ac1bac240542
Damhan91 <damhanp@gmail.com>
2021-10-18 18:13:16 +0000

    collect sales data for last 5 markets in list of columns

--- run.py
+++ run.py
@@ -13,14 +13,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-"""
+
 def get_sales_data():
-    
+    """
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
     via the terminal, which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
-    
+    """
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
@@ -38,11 +38,11 @@ def get_sales_data():
 
 
 def validate_data(values):
-    
+    """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
-    
+    """
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -54,26 +54,7 @@ def validate_data(values):
         return False
 
     return True
-"""
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
 
 def update_worksheet(data, worksheet):
     """
@@ -84,7 +65,8 @@ def update_worksheet(data, worksheet):
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
-    
+
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -103,6 +85,21 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entry_sales():
+    """
+    Collects collumns of data from sales worksheet, 
+    collecting the last 5 entries for each sandwich 
+    and returns the data as a list of lists
+    """
+
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
 
 def main():
     """
@@ -113,6 +110,7 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
